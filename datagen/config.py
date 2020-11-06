c2b1c028e265684a7816c6b18d1612da1e5508ef
Lalu Erfandi Maula Yusnu <nunenuh@gmail.com>
2020-11-06 08:57:57 +0800

    fix: refactor main package

--- datagen/config.py
+++ datagen/config.py
@@ -1,6 +1,10 @@
 
-csv_wilayah = 'data/source/wilayah.csv'
-csv_agama = 'data/source/agama.csv'
-csv_pekerjaan = 'data/source/pekerjaan.csv'
-csv_perkawinan = 'data/source/perkawinan.csv'
-csv_goldarah = 'data/source/goldarah.csv'
+csv_wilayah = 'data/csv_source/wilayah.csv'
+csv_agama = 'data/csv_source/agama.csv'
+csv_pekerjaan = 'data/csv_source/pekerjaan.csv'
+csv_perkawinan = 'data/csv_source/perkawinan.csv'
+csv_goldarah = 'data/csv_source/goldarah.csv'
+csv_nama = 'data/csv_source/nama.csv'
+csv_address = 'data/csv_source/alamat.csv'
+
+random_seed = 1261
