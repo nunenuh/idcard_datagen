849016572088041dff48becf80dcf936a80ba74e
Lalu Erfandi Maula Yusnu <nunenuh@gmail.com>
2021-06-27 23:19:50 +0800

    feat: add white background balance with random sampling feature

--- datagen/imgen/idcard/combiner.py
+++ datagen/imgen/idcard/combiner.py
@@ -17,61 +17,103 @@ from datagen.imgen.io import fop
 
 from tqdm import tqdm
 
+      
+def bg_data_balancer(bg_data, idcard_image_data):
+    if len(idcard_image_data) > len(bg_data):
+        mn_factor = len(idcard_image_data) // len(bg_data)
+        bg_data = bg_data * mn_factor * 2
+        bg_data = random.choices(bg_data, k=len(idcard_image_data))
+    else:
+        bg_data = random.choices(bg_data, k=len(idcard_image_data))
+    return bg_data
 
-def combine(bg_path, idcard_path, dst_path,
-            idcard_ext="png", bg_ext="jpg|png", bg_size=None,
-            angle: int = 30, shear: float = 0.5,
-            scale_ratio: str = "0.3,0.8",
-            num_generated: int = 6,
-            force_resize: bool = False,
-            use_basic_effect: bool = True,
-            basic_effect_mode: str = "simple",
-            use_adv_effect: bool = True,
-            adv_effect_mode: str = "simple"):
+def open_json_file(path):
+    with open(str(path), 'r') as js_file:
+        json_data = json.load(js_file)
+    return json_data
 
-    
-    bg_data, bg_path = clean_background_data(bg_path, bg_ext)
-    idcard_image_data, idcard_json_data = clean_idcard_data(
-        idcard_path, image_ext=idcard_ext
-    )
-    bg_size = clean_background_size(bg_size)
-    dst_path, base_path = clean_destination_path(dst_path)
-    
-    augment_param = clean_augment_param(angle, shear, scale_ratio, num_generated)
-    angle, shear, scale_ratio, num_generated  = augment_param
-    
-    bg_data = bg_data_balancer(bg_data, idcard_image_data)
+def clean_background_data(bg_path, bg_path_ext="jpg|png"):
+    bg_path = Path(bg_path)
+    if not bg_path.exists():
+        raise ValueError(f"Directory path to {str(bg_path)} is not exist!")
+    bg_path_ext = bg_path_ext.split("|")
+    bg_data = []
+    for ext in bg_path_ext:
+        bg_data = bg_data + list(bg_path.glob(f"*.{ext}"))
+    bg_data.sort()
+    print(
+        f'Logs: Loading {len(bg_data)} data from {str(bg_path)} as background')
+    return bg_data, bg_path
+
+def clean_background_size(bg_size):
+    if bg_size != None:
+        if 'x' in bg_size:
+            w , h = bg_size.split('x')
+            bg_size = (int(w),int(h))
+        else:
+            raise ValueError(f"--bg_size format error fill with WxH format, e.g 1200x1000")
+    return bg_size
+
+def add_white_gray_files(bg_path, bg_data, repeat_factor=None,
+                         wg_file = [ 'w1.jpg','w2.jpg','w3.jpg','w4.jpg',
+                                    'gray1.jpg','gray2.jpg','gray3.jpg','gray4.jpg']):
+    bg_wg = []
+    if repeat_factor==None:
+        repeat = int(len(bg_data)/len(wg_file))
+    else:
+        repeat = int(len(bg_data)*repeat_factor/len(wg_file))
+    for i in range(repeat):
+        for fname in wg_file:
+            bgp  = bg_path.joinpath(fname)
+            bg_wg.append(bgp)
+
+    new_bg_data = bg_data + bg_wg
+    random.shuffle(new_bg_data)
+    return new_bg_data
+
+def clean_idcard_data(idcard_path, image_ext="png"):
+    idcard_path = Path(idcard_path)
+    if not idcard_path.exists():
+        raise ValueError(f"Directory path to {str(idcard_path)} is not exist!")
+    image_data = list(idcard_path.glob(f"*.{image_ext}"))
+    json_data = list(idcard_path.glob(f"*.json"))
+
+    image_data.sort()
+    json_data.sort()
+    print(
+        f'Logs: Loading {len(image_data)} data from {str(idcard_path)} as IDCard')
+
+    return image_data, json_data
 
 
-    c = 0
-    tc = len(idcard_image_data) * num_generated
-    print(f'Length of idcard data is {tc}')
-    idcard_bar = tqdm(zip(bg_data, idcard_image_data, idcard_json_data))
-    for (bgfile, idfile, jsfile) in idcard_bar:
-        for n in range(num_generated):
+def clean_destination_path(dst_path):
+    dst_path = Path(dst_path)
+    if not dst_path.exists():
+        raise ValueError(f"Directory path to {str(dst_path)} is not exist!")
+    print(f'Logs: Preparing destination directory at {str(dst_path)}')
+    time_num = int(time.time())
+    base_path = dst_path.joinpath(str(time_num))
+    base_path.mkdir(parents=True, exist_ok=True)
+    print(f'Logs: Creating directory recursively')
 
-            info =  f"Progress ({str(c)}/{str(tc)}): "
-            info += f"Processing augmented ({str(n)}/{str(num_generated)}) saved to {str(base_path)}"
-            idcard_bar.set_description(info)
+    return dst_path, base_path
 
-            data = combine_single(bgfile, idfile, jsfile, base_path, 
-                                  bg_size, scale_ratio, angle, shear,
-                                  force_resize=force_resize,
-                                  use_basic_effect=use_basic_effect,
-                                  basic_effect_mode=basic_effect_mode,
-                                  use_adv_effect=use_adv_effect,
-                                  adv_effect_mode=adv_effect_mode)
-            
-            image_fpath, cmp_img = data['image_path'], data['image_data']
-            mask_fpath, seg_img = data['mask_path'], data['mask_data']
-            json_fpath, json_dict = data['json_path'], data['json_data']
-            
-            
-            cv.imwrite(str(image_fpath), cmp_img)
-            cv.imwrite(str(mask_fpath), seg_img)
-            fop.save_json_file(str(json_fpath), json_dict)
-            c = c + 1
-                
+
+def clean_augment_param(angle, shear, scale_ratio, num_generated):
+    angle = int(angle)
+    shear = float(shear)
+
+    sfactor = 0.1
+    scale_ratio = scale_ratio.split(",")
+    scale_ratio = [float(ratio.strip()) for ratio in scale_ratio]
+    scale_ratio: list = [i for i in np.arange(scale_ratio[0], scale_ratio[1], sfactor)]
+
+    num_generated = int(num_generated)
+    print(f'Info: angle={str(angle)} shear_factor={str(shear)} '
+          f'scale_ratio={str(scale_ratio)}')
+
+    return angle, shear, scale_ratio, num_generated
+
 
 def combine_single(bgfile, idfile, jsfile, base_path: Path,
                    bg_size, scale_ratio, angle, shear, 
@@ -180,82 +222,67 @@ def combine_single(bgfile, idfile, jsfile, base_path: Path,
     }
     
     return data
-      
-def bg_data_balancer(bg_data, idcard_image_data):
-    if len(idcard_image_data) > len(bg_data):
-        mn_factor = len(idcard_image_data) // len(bg_data)
-        bg_data = bg_data * mn_factor * 2
-        bg_data = random.choices(bg_data, k=len(idcard_image_data))
-    else:
-        bg_data = random.choices(bg_data, k=len(idcard_image_data))
-    return bg_data
-
-def open_json_file(path):
-    with open(str(path), 'r') as js_file:
-        json_data = json.load(js_file)
-    return json_data
-
-def clean_background_data(bg_path, bg_path_ext="jpg|png"):
-    bg_path = Path(bg_path)
-    if not bg_path.exists():
-        raise ValueError(f"Directory path to {str(bg_path)} is not exist!")
-    bg_path_ext = bg_path_ext.split("|")
-    bg_data = []
-    for ext in bg_path_ext:
-        bg_data = bg_data + list(bg_path.glob(f"*.{ext}"))
-    bg_data.sort()
-    print(
-        f'Logs: Loading {len(bg_data)} data from {str(bg_path)} as background')
-    return bg_data, bg_path
-
-def clean_background_size(bg_size):
-    if bg_size != None:
-        if 'x' in bg_size:
-            w , h = bg_size.split('x')
-            bg_size = (int(w),int(h))
-        else:
-            raise ValueError(f"--bg_size format error fill with WxH format, e.g 1200x1000")
-    return bg_size
-
-def clean_idcard_data(idcard_path, image_ext="png"):
-    idcard_path = Path(idcard_path)
-    if not idcard_path.exists():
-        raise ValueError(f"Directory path to {str(idcard_path)} is not exist!")
-    image_data = list(idcard_path.glob(f"*.{image_ext}"))
-    json_data = list(idcard_path.glob(f"*.json"))
-
-    image_data.sort()
-    json_data.sort()
-    print(
-        f'Logs: Loading {len(image_data)} data from {str(idcard_path)} as IDCard')
-
-    return image_data, json_data
 
 
-def clean_destination_path(dst_path):
-    dst_path = Path(dst_path)
-    if not dst_path.exists():
-        raise ValueError(f"Directory path to {str(dst_path)} is not exist!")
-    print(f'Logs: Preparing destination directory at {str(dst_path)}')
-    time_num = int(time.time())
-    base_path = dst_path.joinpath(str(time_num))
-    base_path.mkdir(parents=True, exist_ok=True)
-    print(f'Logs: Creating directory recursively')
+def combine(bg_path, idcard_path, dst_path,
+            idcard_ext="png", bg_ext="jpg|png", bg_size=None,
+            balance_white_bg=False, balance_bg=False, sampled_bg=True,
+            angle: int = 30, shear: float = 0.5,
+            scale_ratio: str = "0.3,0.8",
+            num_generated: int = 6,
+            force_resize: bool = False,
+            use_basic_effect: bool = True,
+            basic_effect_mode: str = "simple",
+            use_adv_effect: bool = True,
+            adv_effect_mode: str = "simple"):
 
-    return dst_path, base_path
+    
+    bg_data, bg_path = clean_background_data(bg_path, bg_ext)
+    if balance_white_bg:
+        bg_data = add_white_gray_files(bg_path, bg_data)
+    
+    idcard_image_data, idcard_json_data = clean_idcard_data(
+        idcard_path, image_ext=idcard_ext
+    )
+    bg_size = clean_background_size(bg_size)
+    dst_path, base_path = clean_destination_path(dst_path)
+    
+    augment_param = clean_augment_param(angle, shear, scale_ratio, num_generated)
+    angle, shear, scale_ratio, num_generated  = augment_param
+    
+    if balance_bg:
+        bg_data = bg_data_balancer(bg_data, idcard_image_data)
+        
 
+    c = 0
+    tc = len(idcard_image_data) * num_generated
+    print(f'Length of idcard data is {tc}')
+    idcard_bar = tqdm(zip(bg_data, idcard_image_data, idcard_json_data))
+    for (bgfile, idfile, jsfile) in idcard_bar:
+        if sampled_bg:
+            bgfile = random.sample(bg_data, k=1)[0]
+            
+        for n in range(num_generated):
 
-def clean_augment_param(angle, shear, scale_ratio, num_generated):
-    angle = int(angle)
-    shear = float(shear)
+            info =  f"Progress ({str(c)}/{str(tc)}): "
+            info += f"Processing augmented ({str(n)}/{str(num_generated)}) saved to {str(base_path)}"
+            idcard_bar.set_description(info)
 
-    sfactor = 0.1
-    scale_ratio = scale_ratio.split(",")
-    scale_ratio = [float(ratio.strip()) for ratio in scale_ratio]
-    scale_ratio: list = [i for i in np.arange(scale_ratio[0], scale_ratio[1], sfactor)]
-
-    num_generated = int(num_generated)
-    print(f'Info: angle={str(angle)} shear_factor={str(shear)} '
-          f'scale_ratio={str(scale_ratio)}')
-
-    return angle, shear, scale_ratio, num_generated
+            data = combine_single(bgfile, idfile, jsfile, base_path, 
+                                  bg_size, scale_ratio, angle, shear,
+                                  force_resize=force_resize,
+                                  use_basic_effect=use_basic_effect,
+                                  basic_effect_mode=basic_effect_mode,
+                                  use_adv_effect=use_adv_effect,
+                                  adv_effect_mode=adv_effect_mode)
+            
+            image_fpath, cmp_img = data['image_path'], data['image_data']
+            mask_fpath, seg_img = data['mask_path'], data['mask_data']
+            json_fpath, json_dict = data['json_path'], data['json_data']
+            
+            
+            cv.imwrite(str(image_fpath), cmp_img)
+            cv.imwrite(str(mask_fpath), seg_img)
+            fop.save_json_file(str(json_fpath), json_dict)
+            c = c + 1
+                
